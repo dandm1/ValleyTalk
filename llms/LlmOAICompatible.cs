ef3eee6fed2d70275f717dac8b91dca6bf66182b
dandm1 <david@dandm.online>
2025-01-01 13:14:30 +0000

    Make OpenAiCompatibile tolerant to including too much of the path

--- llms/LlmOAICompatible.cs
+++ llms/LlmOAICompatible.cs
@@ -6,6 +6,10 @@ internal class LlmOAICompatible : LlmOpenAiBase, IGetModelNames
 {
     public LlmOAICompatible(string apiKey, string url, string modelName = null)
     {
+        if (url.EndsWith("/")) url = url.Substring(0, url.Length - 1);
+        if (url.EndsWith("/chat/completions")) url = url.Substring(0, url.Length - 16);
+        if (url.EndsWith("/v1")) url = url.Substring(0, url.Length - 3);
+        
         this.url = url;
 
         this.apiKey = apiKey;
