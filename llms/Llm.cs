a9e06df7d18ee82f13c42abec1da38dd9a333ba1
dandm1 <david@dandm.online>
2025-01-03 16:45:08 +0000

    Fix bug in Llm disabling when connection fails

--- llms/Llm.cs
+++ llms/Llm.cs
@@ -23,7 +23,7 @@ internal abstract class Llm
         };
         Llm instance = CreateInstance(llmType, paramsDict);
         Instance = instance;
-        DialogueBuilder.Instance.LlmDisabled = !CheckConnection(apiKey, modelName);
+        DialogueBuilder.Instance.LlmDisabled = CheckConnection(apiKey, modelName);
     }
 
     private static bool CheckConnection(string apiKey, string modelName)
