0c058164bd0d44c0cdad87290a18ca8eed8a0b2a
dandm1 <david@dandm.online>
2025-05-26 11:53:26 +0100

    Refactor custom intro detection

--- src/PatchNpcCheckAction.cs
+++ src/PatchNpcCheckAction.cs
@@ -10,7 +10,6 @@ namespace ValleyTalk
     [HarmonyPatch(typeof(NPC), nameof(NPC.checkAction))]
     public class PatchNpcCheckAction
     {
-        private static bool wasAltKeyDown = false;
 
         /// <summary>
         /// Key to press while clicking on an NPC to initiate typed dialogue
@@ -22,23 +21,15 @@ namespace ValleyTalk
         /// </summary>
         public static bool Prefix(ref NPC __instance, ref bool __result, Farmer who, GameLocation l)
         {
+
             // Check if the Alt key is being held down (or whatever key is configured)
-            bool isTypedDialogueKeyDown = ModEntry.SHelper.Input.IsDown(InitiateTypedDialogueKey);
+            bool wasTriggerKeyDown = ModEntry.SHelper.Input.IsDown(InitiateTypedDialogueKey);
 
             // If the key is not held down, let the original method handle it
-            if (!isTypedDialogueKeyDown)
+            if (!wasTriggerKeyDown)
             {
-                wasAltKeyDown = false;
                 return true;
             }
-
-            // Prevent multiple triggers from the same key press
-            if (wasAltKeyDown)
-            {
-                return false;
-            }
-
-            wasAltKeyDown = true;
 
             // Make sure we can process dialogue with this NPC
             if (!DialogueBuilder.Instance.PatchNpc(__instance))
