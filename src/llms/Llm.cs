30af970ee47e69fa3e6e61ea1e8fa74a52a8ae5c
dandm1 <david@dandm.online>
2025-08-15 18:59:26 +0100

    Reverse the result on connection check skipping.

--- src/llms/Llm.cs
+++ src/llms/Llm.cs
@@ -30,7 +30,7 @@ internal abstract class Llm
     private static async Task<bool> CheckConnection(string apiKey, string modelName)
     {
         if (ModEntry.Config.SuppressConnectionCheck)
-            return true;
+            return false;
 
         var response = await Instance.RunInference("You are performing LLM connection testing", "Please just ", "respond with ", "'Connection successful'");
         if (response.Length < 5)
